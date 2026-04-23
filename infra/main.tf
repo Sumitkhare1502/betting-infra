b4c3650d39e49afdba1cce44f9de599ee4b223e9
Sumit Khare <sumitkhare1502@@gmail.com>
2026-04-24 02:17:01 +0530

    fixed key pair

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "sg" {
 resource "aws_instance" "ec2" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t2.micro"
-  key_name      = "AKIAWQEU5BPCRRX7DQA5"
+  key_name      = "my-key"
 
   vpc_security_group_ids = [aws_security_group.sg.id]
 
