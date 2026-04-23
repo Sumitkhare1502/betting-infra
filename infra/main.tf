da65b2940c108f365c18eb5805593caef5946aee
Sumit Khare <sumitkhare1502@@gmail.com>
2026-04-24 00:56:36 +0530

    fixed workflow location

---
+++ infra/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "sg" {
+  name = "betting-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  key_name      = "AKIAWQEU5BPCRRX7DQA5"
+
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "betting-ec2"
+  }
+}
