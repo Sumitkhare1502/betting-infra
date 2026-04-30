8f6f0bc69e756c08f1a032f78122f90f06f6482e
Sumit Khare <sumitkhare1502@@gmail.com>
2026-05-01 00:32:49 +0530

    phase 2 terraform setup

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "sg" {
-  name = "betting-sg11"
+  name = "betting-sg-final"
 
   ingress {
     from_port   = 22
@@ -37,11 +37,12 @@ resource "aws_security_group" "sg" {
 resource "aws_instance" "ec2" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t2.micro"
-  key_name = "betting-key"
+
+  key_name = "betting-key"   # ⚠️ वही नाम जो AWS में बनाया
 
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "betting-ec2"
+    Name = "Betting-App-Server"
   }
 }
