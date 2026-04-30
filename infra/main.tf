e12a918868bb4daa13de4c81966a33c580abddcb
Sumit Khare <sumitkhare1502@@gmail.com>
2026-05-01 00:51:39 +0530

    phase 2 terraform setup

--- infra/main.tf
+++ infra/main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_instance" "ec2" {
   ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   key_name = "betting-key"   # ⚠️ वही नाम जो AWS में बनाया
 
